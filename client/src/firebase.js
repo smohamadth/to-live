144cc56321dce7d06765e6ea9b57a717eeaa7447
smohamadth <mohamadfhd32@gmail.com>
2024-02-15 16:53:59 +0330

    added client

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD5Uv4qFlYnXPVQx4sqy1OnjfcRrT1N4rQ",
+  authDomain: "tolive-77063.firebaseapp.com",
+  projectId: "tolive-77063",
+  storageBucket: "tolive-77063.appspot.com",
+  messagingSenderId: "464519296359",
+  appId: "1:464519296359:web:0ab7e01c7877bcb8f26bc2"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ client/src/pages/About.jsx
@@ -0,0 +1,19 @@
+export default function About() {
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font font-semibold text-center my-7'>
+            About To Live
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Welcome to ToLive. Here you can find useful articles and tutorials about how to To Live
+              a happy, fullfiling life
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
