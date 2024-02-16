a51b41037905fe73e3b0a7ae3e12a90d1a431a3a
smohamadth <mohamadfhd32@gmail.com>
2024-02-16 16:03:38 +0330

    some cleaning

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom';
-import CallToAction from '../components/CallToAction';
 import { useEffect, useState } from 'react';
 import PostCard from '../components/PostCard';
 
